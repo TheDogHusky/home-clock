2f2f75500eb96cb6be6a18595629f98f6cad7a18
TheDogHusky <contact@classydev.fr>
2025-05-29 14:55:28 +0200

    fix: active link on the settins pane

--- src/components/SettingsPane.vue
+++ src/components/SettingsPane.vue
@@ -5,15 +5,27 @@ const props = defineProps<{
 const route = useRoute();
 
 const active = ref(props.active);
+const currentCategory = ref(route.query.settingsPaneCategory || 'general');
 
 watch(() => props.active, (newValue) => {
     active.value = newValue;
+});
+watch(() => route.query.settingsPaneCategory, (newValue) => {
+    currentCategory.value = newValue || 'general';
 });
 
 const classes = computed(() => {
     return {
         'settings-pane': true,
         'active': active.value
+    };
+});
+
+const settingsPaneActiveLinkClasses = (category: string) => computed(() => {
+    console.log('currentCategory.value', currentCategory.value, 'category', category);
+    return {
+        'settings-pane-link': true,
+        'active': currentCategory.value === category
     };
 });
 
@@ -28,14 +40,14 @@ const classes = computed(() => {
         />
         <ul class="settings-pane-sidebar">
             <li>
-                <NuxtLink :to="route.path + '?settingsPaneCategory=general'" class="settings-pane-link">
+                <NuxtLink :to="route.path + '?settingsPaneCategory=general'" :class="settingsPaneActiveLinkClasses('general').value">
                     <Icon icon="nf-fa-cog" />
                     <span>General</span>
                 </NuxtLink>
             </li>
             <li>
-                <NuxtLink :to="route.path + '?settingsPaneCategory=appearence'" class="settings-pane-link">
-                    <Icon icon="nf-fa-cog" />
+                <NuxtLink :to="route.path + '?settingsPaneCategory=appearence'" :class="settingsPaneActiveLinkClasses('appearence').value">
+                    <Icon icon="nf-fa-palette" />
                     <span>Appearence</span>
                 </NuxtLink>
             </li>
