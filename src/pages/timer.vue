6aa67c9da35aa97a237f45d540f417f17f96f53d
TheDogHusky <contact@classydev.fr>
2025-05-29 16:39:35 +0200

    feat: add timer creation logic

--- src/pages/timer.vue
+++ src/pages/timer.vue
@@ -11,16 +11,73 @@ const { isFullscreen } = useFullscreen();
 
 const newTimerActive = ref(false);
 const newTimerName = ref('');
-const newTimerDuration = ref('');
+const newTimerHoursDuration = ref('');
+const newTimerMinutesDuration = ref('');
+const newTimerSecondsDuration = ref('');
+const newTimerNameInput = useTemplateRef<HTMLInputElement>('newTimerNameInput');
+const newTimerHoursDurationInput = useTemplateRef<HTMLInputElement>('newTimerHoursDurationInput');
+const newTimerMinutesDurationInput = useTemplateRef<HTMLInputElement>('newTimerMinutesDurationInput');
+const newTimerSecondsDurationInput = useTemplateRef<HTMLInputElement>('newTimerSecondsDurationInput');
 
 const closeNewTimerModal = () => {
     newTimerActive.value = false;
     newTimerName.value = '';
-    newTimerDuration.value = '';
+    newTimerHoursDuration.value = '';
+    newTimerMinutesDuration.value = '';
+    newTimerSecondsDuration.value = '';
 };
 
 function triggerNewTimer() {
     newTimerActive.value = true;
+}
+
+function validateNewTimerInputs() {
+    if (!newTimerName.value.trim()) {
+        newTimerNameInput.value?.classList.add('is-invalid');
+        newTimerNameInput.value?.setCustomValidity('Please enter a timer name.');
+    } else {
+        newTimerNameInput.value?.classList.remove('is-invalid');
+        newTimerNameInput.value?.setCustomValidity('');
+    }
+
+    console.log(newTimerHoursDuration.value, newTimerMinutesDuration.value, newTimerSecondsDuration.value);
+    if (!newTimerHoursDuration.value || isNaN(newTimerHoursDuration.value)) {
+        newTimerHoursDurationInput.value?.classList.add('is-invalid');
+        newTimerHoursDurationInput.value?.setCustomValidity('Please enter a valid number of hours.');
+    } else {
+        newTimerHoursDurationInput.value?.classList.remove('is-invalid');
+        newTimerHoursDurationInput.value?.setCustomValidity('');
+    }
+
+    if (!newTimerMinutesDuration.value || isNaN(newTimerMinutesDuration.value)) {
+        newTimerMinutesDurationInput.value?.classList.add('is-invalid');
+        newTimerMinutesDurationInput.value?.setCustomValidity('Please enter a valid number of minutes.');
+    } else {
+        newTimerMinutesDurationInput.value?.classList.remove('is-invalid');
+        newTimerMinutesDurationInput.value?.setCustomValidity('');
+    }
+
+    if (!newTimerSecondsDuration.value || isNaN(newTimerSecondsDuration.value)) {
+        newTimerSecondsDurationInput.value?.classList.add('is-invalid');
+        newTimerSecondsDurationInput.value?.setCustomValidity('Please enter a valid number of seconds.');
+    } else {
+        newTimerSecondsDurationInput.value?.classList.remove('is-invalid');
+        newTimerSecondsDurationInput.value?.setCustomValidity('');
+    }
+}
+
+function addNewTimer() {
+    validateNewTimerInputs();
+
+    if (newTimerName.value && newTimerHoursDuration.value && newTimerMinutesDuration.value && newTimerSecondsDuration.value) {
+        timersStore.addTimer({
+            name: newTimerName.value,
+            duration: (parseInt(newTimerHoursDuration.value, 10) * 3600) +
+                (parseInt(newTimerMinutesDuration.value, 10) * 60) +
+                parseInt(newTimerSecondsDuration.value, 10),
+        });
+        closeNewTimerModal();
+    }
 }
 </script>
 
@@ -29,14 +86,14 @@ function triggerNewTimer() {
     <ul class="timers">
         <li v-for="timer in timersStore.timers" :key="timer.id" class="timer">
             <span class="timer-name">{{ timer.name }}</span>
-            <span class="timer-time">{{ timer.time }}</span>
-            <button class="btn btn-icon" @click="timer.start">
+            <span class="timer-time"></span>
+            <button class="btn btn-icon" @click="">
                 <i class="nf nf-fa-play"></i>
             </button>
-            <button class="btn btn-icon" @click="timer.stop">
+            <button class="btn btn-icon" @click="">
                 <i class="nf nf-fa-pause"></i>
             </button>
-            <button class="btn btn-icon" @click="timer.reset">
+            <button class="btn btn-icon" @click="">
                 <i class="nf nf-fa-undo"></i>
             </button>
             <button class="btn btn-icon" @click="timersStore.removeTimer(timer.id)">
@@ -55,8 +112,12 @@ function triggerNewTimer() {
         </template>
         <template v-slot:content>
             <form @submit.prevent="">
-                <input v-model="newTimerName" type="text" placeholder="Timer Name" required />
-                <input v-model="newTimerDuration" type="number" placeholder="Duration (seconds)" required />
+                <input ref="newTimerNameInput" v-model="newTimerName" type="text" placeholder="Timer Name" required />
+                <div class="timer-duration">
+                    <span><input ref="newTimerHoursDurationInput" v-model="newTimerHoursDuration" type="number" placeholder="Duration (hours)" required /></span>
+                    <span><input ref="newTimerMinutesDurationInput" v-model="newTimerMinutesDuration" type="number" placeholder="Duration (minutes)" required /></span>
+                    <span><input ref="newTimerSecondsDurationInput" v-model="newTimerSecondsDuration" type="number" placeholder="Duration (seconds)" required /></span>
+                </div>
             </form>
         </template>
         <template v-slot:footer>
