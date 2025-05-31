cafe2a10c2bbd961136b71d49a8c7c3380b7381d
TheDogHusky <contact@classydev.fr>
2025-05-31 22:45:17 +0200

    fix: theme changes in production not working

--- src/app.vue
+++ src/app.vue
@@ -4,16 +4,20 @@ import type { DebuggerEvent } from "vue";
 
 const settingsStore = useSettingsStore();
 // Set the body theme based on the settings automatically
-settingsStore.$subscribe((mutation, state) => {
-    if (((Array.isArray(mutation.events) && mutation.events.some(event => event.key === 'theme')) || (mutation.events as DebuggerEvent).key === 'theme')) {
-        if(state.theme == 'system') {
-            const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-            document.documentElement.setAttribute('data-theme', prefersDark ? 'dark' : 'light');
-        } else document.documentElement.setAttribute('data-theme', state.theme);
-    }
-});
-
 onMounted(() => {
+    // Freak pinia, their docs talk about using subscriptions, with events and payloads, but it doesn't work (no payloads and events are undefined in prod)
+    watch(
+        () => settingsStore.theme,
+        (newTheme) => {
+            const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+            document.documentElement.setAttribute(
+                'data-theme',
+                newTheme === 'system' ? (prefersDark ? 'dark' : 'light') : newTheme
+            );
+        },
+        { immediate: true }
+    );
     // Set the initial theme on mount
     if(settingsStore.theme == 'system') {
         const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
